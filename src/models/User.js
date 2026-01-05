793109b3b671ebaa50f7dfdc159c09859fbd8207
obistafff <currensylla@gmail.com>
2026-01-05 03:36:58 +0100

    Add views middlewares auth

--- src/models/User.js
+++ src/models/User.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    email: { type: String, required: true, unique: true, trim: true, lowercase: true },
-    password: { type: String, required: true }, // hash plus tard si demandé
-    name: { type: String, trim: true }
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true, trim: true }
   },
   { timestamps: true }
 );
